01bf2d32bfe4b5005ead7b00175dbf553d8135f7
C0012S <alwaysh1002@gmail.com>
2022-04-10 20:14:51 +0900

    CodeUp. Python 기초 100제 - 6093 ~ 6094

---
+++ CodeUp/6093.py
@@ -0,0 +1,5 @@
+n = int(input())
+nums = input().split()
+nums.reverse()
+for i in nums:
+    print(int(i), end=' ')
---
+++ CodeUp/6094.py
@@ -0,0 +1,3 @@
+n = int(input())
+nums = map(int, input().split())
+print(min(nums))
