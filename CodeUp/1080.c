cfda19186003747d9a68e561e1e76a72e668cf91
C0012S <alwaysh1002@gmail.com>
2022-07-05 23:35:05 +0900

    CodeUp. C 언어 기초 100제 - 1079 ~ 1080

---
+++ CodeUp/1080.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int max, sum = 0, i = 0;
+    scanf("%d", &max);
+    
+    while (sum < max) {
+        i++;
+        sum += i;
+        //sum += ++i;
+    }
+    
+    printf("%d", i);
+    
+    return 0;
+}
