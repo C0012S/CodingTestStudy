ed2d17cbf5eb453d237c426e7905c3183789546f
C0012S <alwaysh1002@gmail.com>
2022-07-13 23:12:57 +0900

    CodeUp. C 언어 기초 100제 - 1087 ~ 1088

---
+++ CodeUp/1087.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num, sum = 0, i = 0;
+    scanf("%d", &num);
+    
+    while (1) {
+        sum += ++i;
+        
+        if (sum >= num) {
+            printf("%d", sum);
+            break;
+        }
+    }
+  
+    return 0;
+}
